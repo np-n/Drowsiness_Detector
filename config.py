bd322c8ea4565574d068564aed6947fd5138bb9b
np-n <nimesh.neupane43@gmail.com>
2022-09-28 18:05:23 +0545

    configuration parameters

---
+++ config.py
@@ -0,0 +1,10 @@
+"""
+--- Created by : Netra Prasad Neupane
+--- Created on : 9/28/22
+"""
+
+# define two constants, one for the eye aspect ratio to indicate
+# blink and then a second constant for the number of consecutive
+# frames the eye must be below the threshold for to set off the alarm
+EYE_AR_THRESH = 0.2
+EYE_AR_CONSEC_FRAMES = 48
